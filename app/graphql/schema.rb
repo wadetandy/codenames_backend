b38048767f22fdffa2f61623092766c397ad2522
Wade Tandy <wade.tandy@gmail.com>
2020-03-18 02:44:26 -0400

    add error handling

--- app/graphql/schema.rb
+++ app/graphql/schema.rb
@@ -5,4 +5,16 @@ require_relative 'mutation'
 class PartyGamesAppSchema < GraphQL::Schema
   query QueryType
   mutation MutationType
+
+  use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
+  use GraphQL::Execution::Errors
+
+  rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
+    raise GraphQL::ExecutionError, "#{field.type.unwrap.graphql_name} not found"
+  end
+
+  rescue_from(StandardError) do |err, obj, args, ctx, field|
+    raise GraphQL::ExecutionError, "#{err.message}\n#{err.backtrace.join("\n")}"
+  end
 end
